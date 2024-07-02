481a1c023ce91d2518ac5382290c0ba141741ba0
Ana Lina Trejo Morales <ana.trejo@macropay.mx>
2024-07-02 17:48:49 -0600

    agregrar terraform en el bat

--- iniciar_jenkins.bat
+++ iniciar_jenkins.bat
@@ -1,1 +1,2 @@
-docker run -it -p 8090:8080 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -v /root/task2:/task2 --name jenkins_v2 my_jenkins
+docker run -it -p 8090:8080 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -v /root/task2:/task2 --name jen_terr ambos 
+
